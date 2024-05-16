88b6da243c26adc2cc58892007351be4ad47fd53
Margarida Henriques <margaridahenriquesestagio@gmail.com>
2024-05-16 10:10:39 +0100

    first commit

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,100 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+//@desc     Auth user/ set token
+//route     POST api/users/auth
+//@access   Public
+const authUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))){
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+
+});
+
+//@desc     Register a new user
+//route     POST api/users
+//@access   Public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+    
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user){
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
+});
+
+//@desc     Logout user
+//route     POST api/users/logout
+//@access   Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnçy: true,
+        expires: new Date(0)
+    })
+
+    res.status(200).json({ message: 'User logged out'})
+});
+
+//@desc     Get user profile
+//route     GET api/users/profile
+//@access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user ={
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+    };
+    console.log(req.user);
+
+    res.status(200).json({ user})
+});
+
+//@desc     Update user profile
+//route     Put api/users/profile
+//@access   Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Update User'})
+});
+
+
+export{
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+}
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"; 
+const PORT = process.env.PORT || 5555;
+import userRoutes from './routes/userRoutes.js'
+import connectDB from "./config/db.js";
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
+
+app.use('/api/users', userRoutes)
+
+app.get('/' , (req, res) => res.send('Server is ready'))
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT,() => console.log(`Server started one port ${PORT}`))
