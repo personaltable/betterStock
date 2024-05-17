759e93287b4d17557a243383808c740d3a882329
Margarida Henriques <margaridahenriquesestagio@gmail.com>
2024-05-17 11:09:18 +0100

    login_ONGOING_2

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -20,42 +20,48 @@ const Login = () => {
   }
 
   return (
-    <div className='flex h-screen bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500'>
-      <div className='m-auto p-3 flex flex-col gap-3 bg-white rounded-md justify-center items-center'>
-        <div className='p-3'>LOGIN</div>
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-3'>
-          <div>
-            <input
-              {...register("email", {
-                required: "Email é obrigatório",
-                validate: (value) => {
-                  if (!value.includes('@')) {
-                    return "Email inválido";
+    <div className='flex flex-row h-screen bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500'>
+      <div className='m-auto flex flex-row h-80'>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col p-3 px-8 bg-white rounded-l-md justify-between items-center'>
+          <div className='flex flex-col gap-3'>
+            <div className='text-center pt-6'>LOGIN</div>
+
+            <div>
+              <input
+                {...register("email", {
+                  required: "Email é obrigatório",
+                  validate: (value) => {
+                    if (!value.includes('@')) {
+                      return "Email inválido";
+                    }
+                    return true;
                   }
-                  return true;
-                }
-              })}
-              type='text' placeholder='Email'
-              className='border border-gray-300 rounded w-60 h-8 pl-1' />
-            {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+                })}
+                type='text' placeholder='Email'
+                className='border border-gray-300 rounded w-60 h-8 pl-1' />
+              {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+            </div>
+
+            <div>
+              <input
+                {...register("password", {
+                  required: "Password é obrigatória",
+                })}
+                type='password'
+                placeholder='Password'
+                className='border border-gray-300 rounded w-60 h-8 pl-1' />
+              {errors.password && <div className='text-red-500'>{errors.password.message}</div>}
+              <div>Esqueceu a senha?</div>
+            </div>
           </div>
 
-          <div>
-            <input
-              {...register("password", {
-                required: "Password é obrigatória",
-              })}
-              type='password'
-              placeholder='Password'
-              className='border border-gray-300 rounded w-60 h-8 pl-1' />
-            {errors.password && <div className='text-red-500'>{errors.password.message}</div>}
-            <div>Esqueceu a senha?</div>
+          <div className='w-full'>
+            <button type='submit' className={`${isSubmitting ? 'bg-gray-600' : 'bg-black cursor-pointer'} text-white p-1 rounded-md mt-4 w-full`}>Login</button>
+            {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
+            <div>Não tem conta? <Link className="text-blue-600" to="/register">Signup</Link></div>
           </div>
-          <button type='submit' className={`${isSubmitting ? 'bg-gray-600' : 'bg-black cursor-pointer'} text-white p-1 rounded-md`}>Login</button>
-          {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
-          <div>Não tem conta? <Link className="text-blue-600" to="/register">Signup</Link></div>
-
         </form>
+        <div className='bg-black w-80 rounded-r-md'>Welcome</div>
       </div>
     </div>
   )
