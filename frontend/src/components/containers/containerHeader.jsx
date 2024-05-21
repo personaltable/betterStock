2959fc655e23d1f366164d9f816a71debb5adf40
Joao Miguel <joaomiguelabreu11@gmail.com>
2024-05-21 16:56:47 +0100

    DashBoard_Settings_Icon

--- frontend/src/components/containers/containerHeader.jsx
+++ frontend/src/components/containers/containerHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContainerHeader = ({ children, className = '' }) => {
     return (
-        <div className={`p-5 h-28 w-60 bg-white text-center text-2xl shadow-md shadow-slate-700 rounded ${className}`}>
+        <div className={`p-5 h-32 w-64 bg-white text-center text-2xl shadow-md shadow-slate-700 rounded ${className}`}>
             {children}
         </div>
     )
