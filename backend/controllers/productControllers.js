bc91004d47141241056bd69bf4d59980e9866da4
Margarida Henriques <margaridahenriquesestagio@gmail.com>
2024-05-21 15:00:53 +0100

    stock_options_ONGOING

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,13 +1,44 @@
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
+import Category from '../models/categoryModel.js'
 
-//@desc     Auth user/ set token
-//route     POST api/users/auth
+//@desc     Get all products
+//route     GET /api/products/
 //@access   Public
 const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find()
+        .populate('category', 'name')
+        .populate('createdBy', 'name');
 
-       const products = await Product.find(); 
-
+    res.status(200).json(products);
 });
 
-export {getProducts};
+//@desc     Create new product
+//route     POST /api/products/
+//@access   Public
+const createProduct = asyncHandler(async (req, res) => {
+    const { name, categoryName, brand, information, price, createdBy, reStock, lowStock, stock } = req.body;
+
+    const category = await Category.findOne({ name: categoryName });
+    if (!category) {
+        return res.status(400).json({ message: "Category not found" });
+    }
+
+    const product = new Product({
+        name,
+        category: category._id,
+        brand,
+        information,
+        price,
+        createdBy,
+        reStock,
+        lowStock,
+        stock
+    });
+
+    const savedProduct = await product.save();
+
+    res.status(201).json(savedProduct);
+});
+
+export { getProducts, createProduct };
