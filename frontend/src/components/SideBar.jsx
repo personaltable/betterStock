687a35b842c7ddca566a14690a8ff5fd1fb658df
Margarida Henriques <margaridahenriquesestagio@gmail.com>
2024-05-22 09:17:22 +0100

    sideBar_text_ONGOING

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useLogoutMutation } from '../slices/userApiSlice';
 import { logout } from '../slices/authSlice';
-
 
 import { AiFillDashboard } from "react-icons/ai";
 import { GiCardboardBoxClosed } from "react-icons/gi";
@@ -17,35 +16,37 @@ function SideBar() {
 
   const [logoutApiCall] = useLogoutMutation();
 
-  const logoutHandler = async ()=>{
-    try{
+  const logoutHandler = async () => {
+    try {
       await logoutApiCall().unwrap();
       dispatch(logout());
       navigate('/');
-    } catch (err){
+    } catch (err) {
       console.log(err)
     }
   }
 
-  const {userInfo} = useSelector((state) => state.auth)
+  const { userInfo } = useSelector((state) => state.auth)
 
   return (
     <div className='flex flex-col bg-zinc-900 min-w-64 max-w-64 h-screen text-xl p-6 gap-28 text-gray-300'>
 
       <div className='flex flex-row justify-between items-center'>
         <div className='flex flex-row items-center gap-3'>
-          <FaUserCircle/>
-          <div>{userInfo?(userInfo.name) : null}</div>
+          <FaUserCircle className='w-6 h-6' />
+          <div className={`${userInfo.name.length <= 18 ? 'text-lg max-w-32' : 'text-sm'}`}>
+            {userInfo ? userInfo.name : null}
+          </div>
         </div>
         <IoIosLogOut
-        onClick={logoutHandler}
-        className='cursor-pointer'/>
+          onClick={logoutHandler}
+          className='cursor-pointer w-6 h-6' />
       </div>
 
       <div className='flex flex-col gap-3'>
-        <div className='flex flex-row items-center gap-2'><AiFillDashboard/><Link to="/dashboard">DashBoard</Link></div>
-        <div className='flex flex-row items-center gap-2'><GiCardboardBoxClosed/><Link to="/stock">Stock</Link></div>
-        <div className='flex flex-row items-center gap-2'><FaUserCircle/><Link to="/historico">Histórico</Link></div>
+        <div className='flex flex-row items-center gap-2'><AiFillDashboard /><Link to="/dashboard">DashBoard</Link></div>
+        <div className='flex flex-row items-center gap-2'><GiCardboardBoxClosed /><Link to="/stock">Stock</Link></div>
+        <div className='flex flex-row items-center gap-2'><FaUserCircle /><Link to="/historico">Histórico</Link></div>
       </div>
 
     </div>
