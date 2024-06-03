3c54e0a60b51e5eb50ddb4e268d9413d8671766e
Joao Miguel <joaomiguelabreu11@gmail.com>
2024-06-03 11:16:57 +0100

    axios_dashboard

--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 import SideBar from '../components/SideBar';
 import ContainerHeader from '../components/containers/containerHeader';
@@ -8,9 +9,7 @@ import PieChart from '../components/charts/pieChart';
 import BarChart from '../components/charts/barChart';
 import Tooltip from '../components/containers/ToolTip';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useGetProductsQuery } from '../slices/productsApiSlice';
-import { setProducts, setStatus, setError } from '../slices/productsSlice';
+
 
 
 import { IoSettingsOutline } from "react-icons/io5";
@@ -18,20 +17,11 @@ import { CiCircleQuestion } from "react-icons/ci";
 
 
 const Dashboard = () => {
-  const { data: products, error, isLoading } = useGetProductsQuery();
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (isLoading) {
-      dispatch(setStatus('loading'));
-    } else if (error) {
-      dispatch(setError(error));
-      dispatch(setStatus('failed'));
-    } else {
-      dispatch(setProducts(products));
-      dispatch(setStatus('succeeded'));
-    }
-  }, [isLoading, error, products, dispatch]);
+  const fetchData = async () => {
+    const response = await axios.get(`http://localhost:5555/api/products`);
+    console.log(response.data)
+  }
 
   // __________________________Variáveis Dropdown Settings Header____________________________________
   const [columnsOptions, setColumnsOptions] = useState(['Todos os produtos', 'Produtos sem stock', 'Produtos com stock baixo']);
@@ -95,29 +85,45 @@ const Dashboard = () => {
         {/*____________________________ Body___________________________ */}
         <div className='flex flex-row w-full justify-between'>
           <div className='flex flex-wrap justify-start gap-5'>
+
+            {/* -------------------------PIECHART------------------------- */}
             {columnsOptionsBody.includes("Quantidade por categoria(Pie)") ? (
               <ContainerBody>
                 <div className='flex flex-col'>
                   <div className='flex flex-row justify-between'>
                     <div>Quantidade por categoria</div>
-                    <Tooltip content="Este é o tooltip" className='z-20'><CiCircleQuestion /></Tooltip>
+                    <Tooltip content="Grafico Circular que mostra a quantidade de itens por cada categoria" className='z-20'><CiCircleQuestion /></Tooltip>
                   </div>
                   <PieChart />
                 </div>
-
               </ContainerBody>
             ) : (null)}
 
+            {/* -------------------------BARCHART------------------------- */}
             {columnsOptionsBody.includes("Quantidade por categoria(Barras)") ? (
               <ContainerBody>
-                <div>Quantidade por categoria<BarChart /> </div>
+                <div className='flex flex-col'>
+                  <div className='flex flex-row justify-between'>
+                    <div>Quantidade por categoria</div>
+                    <Tooltip content="Grafico de barras que mostra a quantidade de itens por cada categoria" className='z-20'><CiCircleQuestion /></Tooltip>
+                  </div>
+                  <BarChart />
+                </div>
               </ContainerBody>
             ) : (null)}
 
+            {/* ------------------------------------------------------- */}
             {columnsOptionsBody.includes("Produtos stock mais alto") ? (
               <ContainerBody>
-                <div>Produtos com stock mais alto</div>
+                <div className='flex flex-col'>
+                  <div className='flex flex-row justify-between'>
+                    <div>Produtos com stock mais alto</div>
+                    <Tooltip content="Este é o tooltip2" className='z-20'><CiCircleQuestion /></Tooltip>
+                  </div>
+                  jkrgign
+                </div>
               </ContainerBody>
+
             ) : (null)}
 
             {columnsOptionsBody.includes("Produtos stock mais baixo") ? (
