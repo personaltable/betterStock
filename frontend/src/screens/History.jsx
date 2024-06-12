8a3c8a8448671a664fea47b0c5c529fbc1e1b037
Margarida Henriques <margaridahenriquesestagio@gmail.com>
2024-06-12 14:24:59 +0100

    historyPageTable_ALLMOSTDONE

--- frontend/src/screens/History.jsx
+++ frontend/src/screens/History.jsx
@@ -6,6 +6,7 @@ import { useReactTable, flexRender, getCoreRowModel, getSortedRowModel, getFilte
 
 
 import { FaArrowDownShortWide, FaArrowUpWideShort } from 'react-icons/fa6';
+import { IoClose } from "react-icons/io5";
 
 const History = () => {
     const [historyData, setHistoryData] = useState([]);
@@ -24,11 +25,31 @@ const History = () => {
 
     console.log(historyData);
 
-    // Table configuration
+    // Table _________________________________
 
-    const changesOnProduct = () => {
+    const [viewChanges, setViewChanges] = useState(false);
+    const [productChanges, setProductChanges] = useState({});
 
-    }
+    const getAlteredKeys = (original, modified) => {
+
+
+
+        const alteredKeys = [];
+        Object.keys(original).forEach(key => {
+            if (key !== 'createdBy' && original[key] !== modified[key]) {
+                alteredKeys.push(key);
+            }
+        });
+        return alteredKeys;
+    };
+
+    const handleChangesClick = ({ original, modified }) => {
+        setProductChanges({ original, modified });
+        setViewChanges(true);
+    };
+
+
+
 
 
     const [sorting, setSorting] = useState([]);
@@ -39,9 +60,9 @@ const History = () => {
                 id: 'name',
                 header: 'Ação',
                 accessorKey: 'name',
-                cell: (info) => (
-                    <span className={`font-bold ${info.getValue() === 'Adicionar' ? 'text-green-700' : info.getValue() === 'Eliminar' ? 'text-red-700' : info.getValue() === 'Editar' ? 'text-blue-700' : ''}`}>
-                        {info.getValue()}
+                cell: (name) => (
+                    <span className={`font-bold ${name.getValue() === 'Adicionar' ? 'text-green-700' : name.getValue() === 'Eliminar' ? 'text-red-700' : name.getValue() === 'Editar' ? 'text-blue-700' : ''}`}>
+                        {name.getValue()}
                     </span>
                 ),
             },
@@ -54,6 +75,19 @@ const History = () => {
                 id: 'changes',
                 header: 'Alterações',
                 accessorKey: 'changes',
+                cell: (change) => {
+                    const value = change.getValue();
+                    if (!value) {
+                        return <span></span>;
+                    }
+                    const { original, modified } = value;
+                    const alteredKeys = getAlteredKeys(original, modified);
+                    return (
+                        <span onClick={() => handleChangesClick({ original, modified })} className="cursor-pointer text-blue-500">
+                            {alteredKeys.join(', ')}
+                        </span>
+                    );
+                },
             },
             {
                 id: 'user',
@@ -123,6 +157,26 @@ const History = () => {
                         ))}
                     </tbody>
                 </table>
+                {viewChanges &&
+                    <div className={`fixed inset-0 z-50 ${viewChanges ? 'flex' : 'hidden'} justify-center items-center`}>
+                        <div className="fixed inset-0 bg-black opacity-50" onClick={() => setViewChanges(false)}></div>
+                        <div className='relative flex flex-col w-[657px] min-h-[388px] gap-3 p-5 bg-white shadow-lg rounded-lg border border-gray-300 z-50 left-32'>
+                            <div className="flex flex-row justify-between items-center">
+                                <div className="font-bold">Alterações</div>
+                                <IoClose onClick={() => { setViewChanges(false) }} className="flex self-end text-xl cursor-pointer" />
+                            </div>
+                            <ul>
+                                {Object.keys(productChanges.original).map(key => (
+                                    key !== 'createdBy' && productChanges.original[key] !== productChanges.modified[key] && (
+                                        <li key={key}>
+                                            <strong>{key}</strong>: {productChanges.original[key]} &rarr; {productChanges.modified[key]}
+                                        </li>
+                                    )
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     );
